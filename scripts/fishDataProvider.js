1b65546eb932e6f5b7c3e33e96dcf3c589e8faf7
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-07-14 08:57:23 -0500

    added new js file

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,16 @@
+const fishCollection = [
+    {
+        Name: "Martin Jr",
+        Species: "Atlantic Cod",
+        Length: "30 inches",
+        Location: "Atlantic Ocean",
+        Diet: "herring",
+    },
+    {
+        Name: "Bruce",
+        Species: "Great White Shark",
+        Length: "11 feet",
+        Location: "Pacific Ocean",
+        Diet: "fish... But he's trying to be vegetarian",
+    }
+]
